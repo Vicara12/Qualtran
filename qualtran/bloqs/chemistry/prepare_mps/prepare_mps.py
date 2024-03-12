4a599e855623c4e0733f9d198134674a9dc391e2
Víctor Carballo <vicaraxd@gmail.com>
2024-03-12 13:42:39 +0100

    Added documentation

--- qualtran/bloqs/chemistry/prepare_mps/prepare_mps.py
+++ qualtran/bloqs/chemistry/prepare_mps/prepare_mps.py
@@ -15,6 +15,39 @@ from qualtran.bloqs.rotations.phase_gradient import PhaseGradientState
 
 @attrs.frozen
 class PrepareMPS (Bloq):
+    r"""Prepares a quantum state encoded in an MPS.
+
+    Given the tensors that encode a MPS or a Quimb MatrixProductState object, this bloq prepares
+    the equivalent quantum state using the method from [1].
+
+    A phase gradient state is needed for this process. It can be provided externally to improve the
+    T-cost of the algorithm (specially if the process is repeated multiple times), otherwise it is
+    generated and deallocated internally.
+
+    This Bloq can be built directly with the constructor, in which case the tensors that form the
+    MPS must be provided, or from a Quimb MatrixProductState, in which case it is necessary to use
+    the from_quimb_mps method.
+
+    Args:
+        tensors: tuple of tensors (in tuple format) that encode the MPS. The format in the case of
+            a single site MPS is (((coef_0, coef_1),),). For a two site mps the tensor disposition
+            is ([bond_dim_0, physical_ind_0], [bond_dim_0, physical_ind_1]) for the first and second
+            sites. In a n-site MPS the disposition is ([bond_dim_0, physical_ind_0], ...
+            [bond_dim_{i-1}, bond_dim_i, physical_ind_i], ...,
+            [bond_dim_{n-2}, physical_ind_{n-1}]). For an example of this encoding refer to the
+            tutorial.
+        phase_bitsize: size of the register that is used to store the rotation angles when loading
+            the tensor values. Bigger values increase the accuracy of the results but require
+            approximately 2 extra ancilla per qubit of phase_bitsize.
+        uncompute: wether to implement the MPS preparation circuit or its adjoint.
+        internal_phase_gradient: a phase gradient state is needed for the decomposition. It can be
+            either be provided externally if this attribute is set to False or internally otherwise.
+
+    References:
+        [Sequential Generation of Entangled Multiqubit States]
+        (https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.95.110503).
+            C. Schön, E. Solano, F. Verstraete, J. I. Cirac, and M. M. Wolf. 2005.
+    """
     tensors: Tuple[Tuple]
     phase_bitsize: int
     uncompute: bool = False
@@ -52,14 +85,16 @@ class PrepareMPS (Bloq):
         else:
             soqs["phase_grad"] = phase_gradient
         return {"input_state": input_state} | soqs
-    
-    @staticmethod
-    def _fill_gate (gate: ArrayLike) -> ArrayLike:
-        ker = scp.linalg.null_space(gate.T.conj())
-        return np.hstack((gate, ker))
 
     @staticmethod
     def _revert_dims (M: ArrayLike, dims: Tuple[int,...]):
+        r""" Transposes a single dimension of a tensor.
+        For example, in a tensor of shape (4,8,16) reverting the dimension 1 is equivalent to first
+        splitting the dimension 1 into subspaces of size 2, which results in a new shape of
+        (4,2,2,2,16), which for illustration will be labeled as (4,2a,2b,2c,16). Then a transpose
+        of the subspaces that formed the dimension to be reverted is performed, which results in
+        (4,2c,2b,2a,16), and finally a rejoin that leaves the tensor again in the shape (4,8,16).
+        """
         for d in dims:
             shape = M.shape
             wires = (shape[d]-1).bit_length()
@@ -71,9 +106,13 @@ class PrepareMPS (Bloq):
     def _gates_from_tensors (self) -> Tuple[ArrayLike,...]:
         bitsize = len(self.tensors)
         gates = []
+        # if the MPS is a single site long then the only gate is the last one
         if len(self.tensors) > 1:
             gates.append(((0, tuple(np.array(self.tensors[0]).T.reshape((-1)))),))
         for i in range(1,bitsize-1):
+            # the revert dims and rev_i are used to reverse the indices of the bond dimension
+            # used by Quimb (as the convention used by them is the opposite), this means that
+            # the item at bond dimension index 0101 gets transposed to position 1010
             tensor = PrepareMPS._revert_dims(np.array(self.tensors[i]),[1]).T
             gate_cols_data = tensor.reshape((-1,tensor.shape[2])).T
             blen = (gate_cols_data[0].shape[0] - 1).bit_length()
@@ -111,7 +150,7 @@ class PrepareMPS (Bloq):
         transpositions = []
         for i in range(sites):
             transpositions.append([mps[i].inds.index(ind) for ind in corr_inds[i]])
-        # for each site, get its coefficient tensor reordered in the correct format
+        # for each site, get its coefficient tensor reordered according to what was computed before
         reordered = [np.transpose(site.data, transp) for site, transp in zip(mps, transpositions)]
         return [PrepareMPS._tensor_to_tuple(t) for t in reordered]
 
@@ -121,7 +160,7 @@ class PrepareMPS (Bloq):
         r"""Constructs a MPS preparation bloq from a Quimb MPS object.
         Arguments are a Quimb MatrixProductState object and all the others that the default
         constructor of PrepareMPS receives, except for tensors.
-        The bond dimensions of the mps MUST be powers of two.
+        The bond dimensions of the mps must be powers of two.
         """
         mps.compress()
         tensors = PrepareMPS._extract_tensors(mps)
